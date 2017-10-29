813fc61cbdb2a2a92ab73062aec1a2d1b1de507d
shadow_walker <shadowwalkersb@gmail.com>
2017-10-28 22:42:13 -0500

    unicode, unichr

--- libpyEM/qtgui/embrowser.py
+++ libpyEM/qtgui/embrowser.py
@@ -71,14 +71,14 @@ except ImportError:
 
 #---------------------------------------------------------------------------
 try:
-    QString = unicode
+    QString = str
 except NameError:
     # Python 3
     QString = str
 QStringList = list
 
 try:
-    QChar = unichr
+    QChar = chr
 except NameError:
     # Python 3
     QChar = chr
