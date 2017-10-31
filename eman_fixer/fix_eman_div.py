6fd3311617c348e4b8be45fa0626ff4bb3033738
shadow_walker <shadowwalkersb@gmail.com>
2017-10-31 18:33:56 -0500

    report only

--- eman_fixer/fix_eman_div.py
+++ eman_fixer/fix_eman_div.py
@@ -55,7 +55,7 @@ class FixEmanDiv(fixer_base.BaseFix):
                     len(node.children) == 3 and
                 match_division(node.children[1])):
             print "FIXER: lineno: ", node.get_lineno(), "%s %s %s" % (node.children[0], node.children[1], node.children[2])
-            return node
+            return False
         else:
             return False
 
