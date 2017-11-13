31e6b48cee7228db76cb366cba04eb9627f34a5b
shadow_walker <shadowwalkersb@gmail.com>
2017-11-12 20:05:00 -0600

    conftest.py fixture: main_form

--- tests/gui/conftest.py
+++ tests/gui/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+import os
 
 
 def pytest_configure(config):
@@ -12,3 +13,16 @@ def pytest_unconfigure(config):
 @pytest.fixture
 def curdir(request):
     return request.fspath.dirname
+
+def get_main_form(module_name, args=[]):
+    module = __import__(module_name)
+    if not os.path.isdir(module_name):
+        os.mkdir(module_name)
+    main_form = module.main_loop(args)
+    return main_form
+
+@pytest.fixture
+def main_form():
+    # main_form = common.get_main_form()
+    return get_main_form
+    # main_form.close()
