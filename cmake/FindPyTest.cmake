52e9ae7d74642e93d5bff4c9a23d0167d50ffd9c
shadow_walker <shadowwalkersb@gmail.com>
2017-11-23 19:07:12 -0600

    pytest-qt

--- cmake/FindPyTest.cmake
+++ cmake/FindPyTest.cmake
@@ -2,7 +2,9 @@ find_program(PYTEST_EXECUTABLE pytest
 		PATHS ${CONDA_PREFIX}/bin ${CONDA_PREFIX}/Scripts ENV PATH
 		)
 
+find_file(PYTEST_QT  plugin.py ${SP_DIR}/pytestqt)
+
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(PyTest
-		REQUIRED_VARS PYTEST_EXECUTABLE
+		REQUIRED_VARS PYTEST_EXECUTABLE PYTEST_QT
 		)
