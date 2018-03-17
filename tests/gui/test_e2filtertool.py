89a90361eeb08570f3ceedbcfe62c64a9ef66882
shadow_walker <shadowwalkersb@gmail.com>
2018-03-16 20:12:43 -0500

    win.cycle in test_* scripts

--- tests/gui/test_e2filtertool.py
+++ tests/gui/test_e2filtertool.py
@@ -4,3 +4,11 @@ import os
 
 def test_mouseClick_altModifier(qtbot, win, curdir):
     win = win('e2filtertool','%s'%os.path.join(curdir,'e2display/twod.hdf'))
+    main_form = win.main_form
+    qtbot.addWidget(main_form)
+    
+    win.cycle(qtbot, main_form)
+    win.cycle(qtbot, main_form.viewer[0])
+    qtbot.mouseClick(main_form.viewer[0], Qt.LeftButton, Qt.AltModifier)
+    qtbot.wait(1000)
+# Need a set of filters and filter types to test spinboxes, checkboxes and buttons
