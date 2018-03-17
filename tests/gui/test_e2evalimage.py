09d71395c87088afa9b73eb2313af79689e9a622
shadow_walker <shadowwalkersb@gmail.com>
2018-03-16 20:12:43 -0500

    win.cycle in test_* scripts

--- tests/gui/test_e2evalimage.py
+++ tests/gui/test_e2evalimage.py
@@ -3,3 +3,10 @@ import os
 
 def test_display_file(qtbot, win, curdir):
     win = win('e2evalimage',[os.path.join(curdir, 'e2evalimage', 'BGal_000232.hdf')])
+    main_form = win.main_form
+    qtbot.addWidget(main_form)
+    
+    win.cycle(qtbot, main_form)
+    win.cycle(qtbot, main_form.wplot)
+    win.cycle(qtbot, main_form.wfft)
+    win.cycle(qtbot, main_form.wimage)
