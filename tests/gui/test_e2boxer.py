acc0253a8af0446a8a81239ae50c0bb3ece532a3
shadow_walker <shadowwalkersb@gmail.com>
2018-03-16 20:12:43 -0500

    win.cycle in test_* scripts

--- tests/gui/test_e2boxer.py
+++ tests/gui/test_e2boxer.py
@@ -3,3 +3,9 @@ from PyQt4.QtCore import Qt
 
 def test_cli(qtbot, win, curdir):
     win = win('e2boxer', apix=1, imagenames=["1\t%s"%os.path.join(curdir,'e2boxer', 'test_box.hdf')])
+    main_form = win.main_form
+    qtbot.addWidget(main_form)
+    
+    win.cycle(qtbot, main_form)
+    win.cycle(qtbot, main_form.wimage, Qt.LeftButton)
+    win.cycle(qtbot, main_form.wparticles)
