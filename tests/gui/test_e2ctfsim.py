0d0988ad7198377b00bfab9b5e7e9e424b6c5443
shadow_walker <shadowwalkersb@gmail.com>
2018-03-16 20:12:43 -0500

    win.cycle in test_* scripts

--- tests/gui/test_e2ctfsim.py
+++ tests/gui/test_e2ctfsim.py
@@ -1,2 +1,8 @@
 def test_cli(qtbot, win):
     win = win('e2ctfsim','')
+    main_form = win.main_form
+    qtbot.addWidget(main_form)
+    
+    win.cycle(qtbot, main_form)
+    win.cycle(qtbot, main_form.guiim)
+    win.cycle(qtbot, main_form.guiplot)
--- tests/gui/test_e2history.py
+++ tests/gui/test_e2history.py
@@ -3,3 +3,7 @@ from PyQt4.QtCore import Qt
 
 def test_okButton(qtbot, win):
     win = win('e2history')
+    main_form = win.main_form
+    
+    win.cycle(qtbot, main_form.form)
+    qtbot.mouseClick(main_form.form.layout().itemAt(1).itemAt(1).widget(), Qt.LeftButton)
