a1b013deedbb1027852e1b87ea834de94a8fd361
shadow_walker <shadowwalkersb@gmail.com>
2018-03-23 11:10:28 -0500

    e2display skipped tests to be implemented

--- tests/gui/test_e2display.py
+++ tests/gui/test_e2display.py
@@ -7,3 +7,34 @@ def test_display_file(qtbot, win):
     qtbot.addWidget(main_form)
 
     win.cycle(qtbot, main_form)
+
+# Tabs inspector when Option + LeftClick
+# Save
+@pytest.mark.skip(reason="Tests not implemented yet")
+def test_display_tab_save():
+    pass
+
+# Probe
+@pytest.mark.skip(reason="Tests not implemented yet")
+def test_display_tab_probe():
+    pass
+
+# Meas
+@pytest.mark.skip(reason="Tests not implemented yet")
+def test_display_tab_meas():
+    pass
+
+# Draw
+@pytest.mark.skip(reason="Tests not implemented yet")
+def test_display_tab_draw():
+    pass
+
+# PSpec
+@pytest.mark.skip(reason="Tests not implemented yet")
+def test_display_tab_pspec():
+    pass
+
+# Python
+@pytest.mark.skip(reason="Tests not implemented yet")
+def test_display_tab_python():
+    pass
