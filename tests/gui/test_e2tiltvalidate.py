cbca80c890f49fbd55a8f67246cbd02bce6bdbe2
shadow_walker <shadowwalkersb@gmail.com>
2018-03-24 19:23:09 -0500

    e2tiltvalidate stash

--- tests/gui/test_e2tiltvalidate.py
+++ tests/gui/test_e2tiltvalidate.py
@@ -4,6 +4,11 @@ from PyQt4.QtCore import Qt
 def test_cli(qtbot, win, datadir):
     win = win('e2tiltvalidate', os.path.join(datadir, 'e2tiltvalidate', 'TiltValidate_04'), -1, 360.0, plotdatalabels=False, color='#00ff00', plotzaxiscolor=False)
     
+    --volume 3DmapIP3R1_clip_188apix.mrc 
+    --untiltdata Ip3R_particles_02_0deg_ptcls_ctfflip.hdf 
+    --tiltdata Ip3R_particles_02_10deg_ptcls_ctfflip.hdf 
+    --path TiltValidate_04
+    --gui
     main_form = win.main_form
     qtbot.addWidget(main_form)
     
