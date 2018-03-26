bd8732e35759ffb5f3c1d1a96459094776ed097d
shadow_walker <shadowwalkersb@gmail.com>
2018-03-26 18:17:30 -0500

    clickButton: e2boxer

--- tests/gui/test_e2boxer.py
+++ tests/gui/test_e2boxer.py
@@ -7,5 +7,11 @@ def test_cli(qtbot, win, datadir):
     qtbot.addWidget(main_form)
     
     win.cycle(qtbot, main_form)
+    win.clickButton(qtbot, main_form, main_form.bmdel)
+    win.clickButton(qtbot, main_form, main_form.bmgref)
+    win.clickButton(qtbot, main_form, main_form.bmbref)
+    win.clickButton(qtbot, main_form, main_form.bmbgref)
+    win.clickButton(qtbot, main_form, main_form.bmmanual)
+    
     win.cycle(qtbot, main_form.wimage, Qt.LeftButton)
     win.cycle(qtbot, main_form.wparticles)
