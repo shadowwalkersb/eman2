278c19a2f8a224f9df18cd4cf51a104303961f17
shadow_walker <shadowwalkersb@gmail.com>
2018-03-26 18:37:19 -0500

    e2ctfsim

--- tests/gui/test_e2ctfsim.py
+++ tests/gui/test_e2ctfsim.py
@@ -6,3 +6,9 @@ def test_cli(qtbot, win):
     win.cycle(qtbot, main_form)
     win.cycle(qtbot, main_form.guiim)
     win.cycle(qtbot, main_form.guiplot)
+    
+    for f in [main_form.sdefocus, main_form.sbfactor, main_form.sampcont]:
+        win.setValSlider(qtbot, main_form, f)
+        win.cycle(qtbot, main_form)
+        win.cycle(qtbot, main_form.guiim)
+        win.cycle(qtbot, main_form.guiplot)
