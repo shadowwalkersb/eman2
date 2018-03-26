1da096cfb7d6c0ecb63f5e0e784febf313bd15fb
shadow_walker <shadowwalkersb@gmail.com>
2018-03-26 15:10:03 -0500

    rename

--- tests/gui/test_e2spt_wedge.py
+++ tests/gui/test_e2spt_wedge.py
@@ -2,7 +2,7 @@ import os
 
 
 def test_display_initial_gui(qtbot, win, curdir):
-    stack=os.path.join(curdir, "e2spt_wedge", "e15ref_raw.hdf")
+    stack=os.path.join(curdir, "e2spt_wedge", "00bin16.hdf")
     
     win = win('e2spt_wedge', stack, 60.0, 0.05, 0.5)
     main_form = win.main_form
