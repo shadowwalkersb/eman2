5e8bb94b78226e833b2de3f2bac9004952ea1bc9
shadow_walker <shadowwalkersb@gmail.com>
2018-03-28 15:17:35 -0500

    add cmake target: test-gui-coverage

--- cmake/FindPyTest.cmake
+++ cmake/FindPyTest.cmake
@@ -3,8 +3,9 @@ find_program(PYTEST_EXECUTABLE pytest
 		)
 
 find_file(PYTEST_QT  plugin.py ${SP_DIR}/pytestqt)
+find_file(PYTEST_COV plugin.py ${SP_DIR}/pytest_cov)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(PyTest
-		REQUIRED_VARS PYTEST_EXECUTABLE PYTEST_QT
+		REQUIRED_VARS PYTEST_EXECUTABLE PYTEST_QT PYTEST_COV
 		)
