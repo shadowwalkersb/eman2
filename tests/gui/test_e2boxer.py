97bb4f15c427a6a1a6922cc9de51e4f4c756ebc7
shadow_walker <shadowwalkersb@gmail.com>
2018-03-30 15:44:31 -0500

    update input dir

--- tests/gui/test_e2boxer.py
+++ tests/gui/test_e2boxer.py
@@ -2,7 +2,7 @@ import os
 from PyQt4.QtCore import Qt
 
 def test_cli(qtbot, win, curdir):
-    win = win('e2boxer', apix=1, imagenames=["1\t%s"%os.path.join(curdir,'e2boxer', 'test_box.hdf')], box=32)
+    win = win('e2boxer', apix=1, imagenames=["1\t%s"%os.path.join(curdir, 'data', 'e2boxer', 'test_box.hdf')], box=32)
     main_form = win.main_form
     qtbot.addWidget(main_form)
     
