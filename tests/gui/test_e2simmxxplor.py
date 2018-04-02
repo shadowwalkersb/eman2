6b9c15028a57ef8022613616ef25e56065cae169
shadow_walker <shadowwalkersb@gmail.com>
2018-04-02 18:32:12 -0500

    e2simmxxplor

---
+++ tests/gui/test_e2simmxxplor.py
@@ -0,0 +1,16 @@
+import os
+
+def test_display_initial_gui(qtbot, win, datadir):
+    
+    root_dir = os.path.join(datadir, "playground")
+    args=[
+        os.path.join(root_dir, "refine_01", "simmx_01_even.hdf"),
+        os.path.join(root_dir, "refine_01", "projections_01_even.hdf"),
+        os.path.join(root_dir, "sets", "all__ctf_flip_lp14_even.lst"),
+    ]
+    
+    win = win('e2simmxxplor', args[0], args[1], args[2])
+    main_form = win.main_form
+    qtbot.addWidget(main_form)
+
+    win.cycle(qtbot, main_form)
