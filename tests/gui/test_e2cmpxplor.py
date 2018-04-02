6473ee1b3b1a8487f3b30d62089cd232d92e48f4
shadow_walker <shadowwalkersb@gmail.com>
2018-04-02 13:43:51 -0500

    update input dir

--- tests/gui/test_e2cmpxplor.py
+++ tests/gui/test_e2cmpxplor.py
@@ -3,8 +3,8 @@ import os
 
 def test_display_initial_gui(qtbot, win, curdir):
     args=[
-        os.path.join(curdir, "e2cmpxplor", "projections_02_odd.hdf"),
-        os.path.join(curdir, "e2cmpxplor", "BGal_000232.hdf"),
+        os.path.join(curdir, "data", "e2cmpxplor", "projections_02_odd.hdf"),
+        os.path.join(curdir, "data", "e2cmpxplor", "BGal_000232.hdf"),
     ]
     win = win('e2cmpxplor', args[0], args[1])
     main_form = win.main_form
