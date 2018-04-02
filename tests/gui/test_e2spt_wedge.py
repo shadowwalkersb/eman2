96c3c22bb5ddaa0dc2b5862647834b07da18e0e5
shadow_walker <shadowwalkersb@gmail.com>
2018-04-02 14:25:58 -0500

    update input dir

--- tests/gui/test_e2spt_wedge.py
+++ tests/gui/test_e2spt_wedge.py
@@ -2,7 +2,7 @@ import os
 
 
 def test_display_initial_gui(qtbot, win, curdir):
-    stack=os.path.join(curdir, "e2spt_wedge", "00bin16.hdf")
+    stack=os.path.join(curdir, "data", "e2spt_wedge", "00bin16.hdf")
     
     win = win('e2spt_wedge', stack, 60.0, 0.05, 0.5)
     main_form = win.main_form
