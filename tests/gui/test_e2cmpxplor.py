508e2aa23d854f2a9c645b595c747bbbb605cb3b
shadow_walker <shadowwalkersb@gmail.com>
2018-04-22 15:57:39 -0500

    extract module name

--- tests/gui/test_e2cmpxplor.py
+++ tests/gui/test_e2cmpxplor.py
@@ -1,12 +1,14 @@
 import os
 
+module_name = 'e2cmpxplor'
+
 
 def test_display_initial_gui(qtbot, win, datadir):
     args=[
         os.path.join(datadir, "e2cmpxplor", "projections_02_odd.hdf"),
         os.path.join(datadir, "e2cmpxplor", "BGal_000232.hdf"),
     ]
-    win = win('e2cmpxplor', args[0], args[1])
+    win = win(module_name, args[0], args[1])
     main_form = win.main_form
     qtbot.addWidget(main_form)
 
