e568312612dd30b574c73c164582a89978e809ae
shadow_walker <shadowwalkersb@gmail.com>
2018-04-22 15:58:00 -0500

    test_display_data

--- tests/gui/test_e2cmpxplor.py
+++ tests/gui/test_e2cmpxplor.py
@@ -13,3 +13,16 @@ def test_display_initial_gui(qtbot, win, datadir):
     qtbot.addWidget(main_form)
 
     win.cycle(qtbot, main_form)
+
+def test_display_data(qtbot, win, datadir):
+    root_dir = os.path.join(datadir, "playground")
+    args=[
+        os.path.join(root_dir, "refine_01", "projections_01_even.hdf"),
+        os.path.join(root_dir, "particles", "BGal_000232__ctf_flip_lp14.hdf"),
+    ]
+    
+    win = win(module_name, args[0], args[1])
+    main_form = win.main_form
+    qtbot.addWidget(main_form)
+
+    win.cycle(qtbot, main_form)
