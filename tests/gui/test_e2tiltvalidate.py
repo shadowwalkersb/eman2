be7e7cd4d579b6c1c2fadb83f3630471834bcecc
shadow_walker <shadowwalkersb@gmail.com>
2018-04-21 23:36:14 -0500

    curdir -> datadir

--- tests/gui/test_e2tiltvalidate.py
+++ tests/gui/test_e2tiltvalidate.py
@@ -1,8 +1,8 @@
 import os
 from PyQt4.QtCore import Qt
 
-def test_cli(qtbot, win, curdir):
-    win = win('e2tiltvalidate', os.path.join(curdir, 'data', 'e2tiltvalidate', 'TiltValidate_04'), -1, 360.0, plotdatalabels=False, color='#00ff00', plotzaxiscolor=False)
+def test_cli(qtbot, win, datadir):
+    win = win('e2tiltvalidate', os.path.join(datadir, 'e2tiltvalidate', 'TiltValidate_04'), -1, 360.0, plotdatalabels=False, color='#00ff00', plotzaxiscolor=False)
     
     main_form = win.main_form
     qtbot.addWidget(main_form)
