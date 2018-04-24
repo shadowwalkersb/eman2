ae81f3066cff46aeccb42a8fb94c75ec3002a40d
shadow_walker <shadowwalkersb@gmail.com>
2018-04-24 00:29:29 -0500

    STASH

---
+++ tests/gui/test_e2ctf.py
@@ -0,0 +1,1 @@
+e2ctf.py particles/BGal_000232.hdf particles/BGal_000233.hdf particles/BGal_000236.hdf particles/BGal_000237.hdf particles/BGal_000247.hdf --allparticles --sortdefocus --minptcl=0 --minqual=0 --gui --constbfactor=-1.0 --sf=auto
