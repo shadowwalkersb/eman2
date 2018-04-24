221c4bfdcfb25cfcef6a5c203ec4f0fed7a4b82d
shadow_walker <shadowwalkersb@gmail.com>
2018-04-24 00:38:10 -0500

    sq

--- tests/gui/test_e2ctf.py
+++ tests/gui/test_e2ctf.py
@@ -1,1 +1,2 @@
-e2ctf.py particles/BGal_000232.hdf particles/BGal_000233.hdf particles/BGal_000236.hdf particles/BGal_000237.hdf particles/BGal_000247.hdf --allparticles --sortdefocus --minptcl=0 --minqual=0 --gui --constbfactor=-1.0 --sf=auto
+
+e2ctf.py particles/*.hdf --allparticles --sortdefocus --minptcl=0 --minqual=0 --gui --constbfactor=-1.0 --sf=auto
