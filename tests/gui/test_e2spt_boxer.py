822136502eee24a23082ccf31569c0e4fa2518e2
shadow_walker <shadowwalkersb@gmail.com>
2018-04-24 23:54:05 -0500

    e2spt_boxer

--- tests/gui/test_e2spt_boxer.py
+++ tests/gui/test_e2spt_boxer.py
@@ -1,13 +1,7 @@
 import os
 
 def test_display_initial_gui(qtbot, win, datadir):
-    datafile = os.path.join(datadir, "e2spt_boxer", "00bin32.hdf")
-    options = type('', (), {})
-    options.apix = 0.0
-    options.invert = False
-    options.inmemory = False
-
-    win = win('e2spt_boxer', options, datafile)
+    win = win('e2spt_boxer', [os.path.join(datadir, "e2spt_boxer", "00bin32.hdf")])
     main_form = win.main_form
 
     win.cycle(qtbot, main_form)
