fa9424f2877f8db3ab9db47565e4298a52c2483e
shadow_walker <shadowwalkersb@gmail.com>
2018-04-24 23:31:18 -0500

    e2filtertool

--- tests/gui/test_e2filtertool.py
+++ tests/gui/test_e2filtertool.py
@@ -3,7 +3,7 @@ import os
 
 
 def test_mouseClick_altModifier(qtbot, win, datadir):
-    win = win('e2filtertool','%s'%os.path.join(datadir, 'e2display/twod.hdf'))
+    win = win('e2filtertool', [os.path.join(datadir, 'e2display', 'twod.hdf')])
     main_form = win.main_form
     qtbot.addWidget(main_form)
     
