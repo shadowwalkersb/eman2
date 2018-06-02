755dc6fc1548ffaa8f15d2896017bccb2371619e
shadow_walker <shadowwalkersb@gmail.com>
2018-06-02 05:20:18 -0500

    new button, down key, Key_Alt

--- tests/gui/test_e2ctfsim.py
+++ tests/gui/test_e2ctfsim.py
@@ -1,8 +1,15 @@
+from PyQt4.QtCore import Qt
+
+
 def test_cli(qtbot, win):
     win = win('e2ctfsim')
     main_form = win.main_form
     qtbot.addWidget(main_form)
     
     win.cycle(qtbot, main_form)
+    win.clickButton(qtbot, main_form, main_form.newbut)
+    qtbot.keyPress(main_form.setlist, Qt.Key_Down)
+    qtbot.keyPress(main_form.setlist, Qt.Key_Alt)
+    
     win.cycle(qtbot, main_form.guiim)
     win.cycle(qtbot, main_form.guiplot)
