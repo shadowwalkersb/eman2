10680af78ad52c47d02e5c4c1144bde6d6d8431b
shadow_walker <shadowwalkersb@gmail.com>
2018-06-05 00:54:51 -0500

    em3Dfonts

--- tests/gui/test_qtgui.py
+++ tests/gui/test_qtgui.py
@@ -1,5 +1,12 @@
+import pytest
+
+from eman2_gui import em3Dfonts
 from eman2_gui import emlights 
 
+
+@pytest.mark.qt_no_exception_capture
+def test_em3Dfonts():
+    em3Dfonts.main()
 
 def test_emlights():
     emlights.main()
