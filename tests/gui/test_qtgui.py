1c5878af9f41a8c96570aff84f61d8bef98993a2
shadow_walker <shadowwalkersb@gmail.com>
2018-06-05 00:54:51 -0500

    emboxerbase

--- tests/gui/test_qtgui.py
+++ tests/gui/test_qtgui.py
@@ -1,12 +1,17 @@
+import os
 import pytest
 
 from eman2_gui import em3Dfonts
+from eman2_gui import emboxerbase 
 from eman2_gui import emlights 
 
 
 @pytest.mark.qt_no_exception_capture
 def test_em3Dfonts():
     em3Dfonts.main()
+
+def test_emboxerbase(datadir):
+    emboxerbase.main([os.path.join(datadir, 'e2boxer', 'test_box.hdf')])
 
 def test_emlights():
     emlights.main()
