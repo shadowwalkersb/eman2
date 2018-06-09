943e8fd0548f07fe7894534521f4bb5383b61150
shadow_walker <shadowwalkersb@gmail.com>
2018-06-09 01:29:19 -0700

    working

--- tests/gui/test_e2tiltvalidate.py
+++ tests/gui/test_e2tiltvalidate.py
@@ -1,6 +1,7 @@
 import os
-from PyQt4.QtCore import Qt
+from PyQt4.QtCore import Qt, QPoint
 import pytest
+import pyautogui
 
 
 @pytest.mark.xfail
@@ -11,3 +12,14 @@ def test_cli(qtbot, win, datadir):
     qtbot.addWidget(main_form)
     
     win.cycle(qtbot, main_form)
+    
+    plot = main_form.polarplot
+    # qtbot.mouseMove(plot, QPoint(20, 20))
+    # qtbot.mouseClick(plot, Qt.LeftButton)
+    # pyautogui.click()
+    # qtbot.wait(1000)
+    qtbot.mouseMove(plot, QPoint(0, 0))
+    # qtbot.wait(1000)
+    # qtbot.mouseRelease(plot, Qt.LeftButton, delay=100)
+    pyautogui.dragRel(100,100, .1)
+    qtbot.wait(1000)
