f8402a1d2c5587511180f73fdf3bf0829f76522d
shadow_walker <shadowwalkersb@gmail.com>
2018-06-11 18:40:37 -0700

    press alt

--- tests/gui/test_e2spt_boxer.py
+++ tests/gui/test_e2spt_boxer.py
@@ -1,9 +1,27 @@
 import os
+from PyQt4.QtCore import Qt
+import pyautogui
+
 
 def test_display_initial_gui(qtbot, win, datadir):
     win = win('e2spt_boxer', [os.path.join(datadir, "e2spt_boxer", "00bin32.hdf")])
     main_form = win.main_form
 
     win.cycle(qtbot, main_form)
+    # qtbot.wait(1000)
+    # qtbot.keyPress(main_form, Qt.Key_Alt)
+    # qtbot.wait(1000)
+    # qtbot.keyPress(main_form, Qt.Key_Shift, delay=1)
+    # qtbot.wait(1000)
+    # qtbot.keyPress(main_form, Qt.Key_Control, delay=1)
+    # qtbot.wait(100)
+    pyautogui.press('alt')
+    qtbot.wait(100)
+    # pyautogui.press('shift')
+    
     win.cycle(qtbot, main_form.boxesviewer)
+    # # qtbot.keyPress(main_form.boxesviewer, Qt.Key_Alt)
+    # 
     win.cycle(qtbot, main_form.optionviewer)
+    # # qtbot.keyPress(main_form.optionviewer, Qt.Key_Alt)
+
