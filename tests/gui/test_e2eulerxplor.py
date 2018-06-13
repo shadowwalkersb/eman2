7583805a48410b3135c9658cebe813b06daea5e6
shadow_walker <shadowwalkersb@gmail.com>
2018-06-13 01:14:46 -0700

    e2eulerxplor

--- tests/gui/test_e2eulerxplor.py
+++ tests/gui/test_e2eulerxplor.py
@@ -1,5 +1,7 @@
 from PyQt4.QtCore import Qt
 import pytest
+from PyQt4.QtCore import Qt, QPoint
+import pyautogui
 
 
 @pytest.mark.qt_no_exception_capture
@@ -9,3 +11,18 @@ def test_cli(qtbot, win):
     qtbot.addWidget(main_form)
     
     win.cycle(qtbot, main_form)
+    # main_form = main_form.model
+    # pyautogui.click()
+
+    # qtbot.keyPress(main_form, Qt.Key_Down)
+    # qtbot.keyPress(main_form, Qt.Key_Alt)
+
+    # qtbot.mouseMove(main_form, QPoint(0, 0))
+    # pyautogui.click()
+    # pyautogui.dragRel(10,10, .1)
+
+    # win.cycle(qtbot, main_form.guiplot)
+    # win.clickButton(qtbot, main_form, main_form.guiplot)
+    # qtbot.mouseMove(main_form.guiplot, QPoint(0, 0))
+    # pyautogui.click()
+    # pyautogui.dragRel(10,10, .1)
