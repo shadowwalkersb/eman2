541cb8951ead15c096eeed78cb50404fa92dbc06
shadow_walker <shadowwalkersb@gmail.com>
2018-06-13 01:47:26 -0700

    e2evalparticles

--- tests/gui/test_e2evalparticles.py
+++ tests/gui/test_e2evalparticles.py
@@ -1,6 +1,18 @@
+from PyQt4.QtCore import Qt
+# from PyQt4.QtCore import Qt, QPoint
+# import pyautogui
+
+
 def test_display_initial_gui(qtbot, win):
     win = win('e2evalparticles', ['--gui'])
     main_form = win.main_form
     qtbot.addWidget(main_form)
 
     win.cycle(qtbot, main_form)
+    main_form = main_form.wclasstab
+    qtbot.keyPress(main_form.wfilesel, Qt.Key_Down)
+    qtbot.keyPress(main_form.wfilesel, Qt.Key_Alt)
+    qtbot.wait(500)
+    win.clickButton(qtbot, main_form, main_form.wfilesel)
+    win.clickButton(qtbot, main_form, main_form.wselallb)
+    win.clickButton(qtbot, main_form, main_form.wselnoneb)
