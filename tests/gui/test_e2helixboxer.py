781d36c3e7602b5ecfa792f2490e0cce5373e36c
shadow_walker <shadowwalkersb@gmail.com>
2018-06-13 02:05:18 -0700

    e2helixboxer

--- tests/gui/test_e2helixboxer.py
+++ tests/gui/test_e2helixboxer.py
@@ -1,5 +1,6 @@
-from PyQt4.QtCore import Qt
+from PyQt4.QtCore import Qt, QPoint
 import os
+import pyautogui
 
 
 def test_display_initial_gui(qtbot, win, datadir):
@@ -8,6 +9,12 @@ def test_display_initial_gui(qtbot, win, datadir):
     
     win.cycle(qtbot, main_form.main_image)
     qtbot.mouseClick(main_form.main_image, Qt.LeftButton)
+
+    qtbot.mouseMove(main_form.main_image, QPoint(10, 10))
+    # qtbot.mousePress(main_form.main_image, Qt.LeftButton, pos=QPoint(0, 0))
+    pyautogui.click()
+    pyautogui.dragRel(100,100, .1)
+    qtbot.wait(300)
 
     # main_window = main_window('e2helixboxer', [os.path.join(datadir, 'e2boxer', 'test_box.hdf'), "--gui"])
     # snap = snap('e2helixboxer')
