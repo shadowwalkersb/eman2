f4a4d56c5504da77565f07f186ce9a40dd1d033b
shadow_walker <shadowwalkersb@gmail.com>
2018-06-13 01:30:45 -0700

    e2evalimage

--- tests/gui/test_e2evalimage.py
+++ tests/gui/test_e2evalimage.py
@@ -1,5 +1,6 @@
 import os
-from PyQt4.QtCore import Qt
+from PyQt4.QtCore import Qt, QPoint
+import pyautogui
 
 
 def test_display_file(qtbot, win, datadir):
@@ -10,7 +11,24 @@ def test_display_file(qtbot, win, datadir):
     win.cycle(qtbot, main_form)
     qtbot.keyPress(main_form.setlist, Qt.Key_Down)
     qtbot.keyPress(main_form.setlist, Qt.Key_Alt)
+    # win.clickButton(qtbot, main_form, main_form.brefit)
 
     win.cycle(qtbot, main_form.wplot)
+    win.clickButton(qtbot, main_form, main_form.wplot)
+    qtbot.mouseMove(main_form.wplot, QPoint(0, 0))
+    pyautogui.click()
+    pyautogui.dragRel(10,10, .1)
+
     win.cycle(qtbot, main_form.wfft)
+    win.clickButton(qtbot, main_form, main_form.wfft)
+    qtbot.mouseMove(main_form.wfft, QPoint(0, 0))
+    pyautogui.click()
+    pyautogui.dragRel(10,10, .1)
+
     win.cycle(qtbot, main_form.wimage)
+    win.clickButton(qtbot, main_form, main_form.wimage)
+    qtbot.mouseMove(main_form.wimage, QPoint(0, 0))
+    pyautogui.click()
+    pyautogui.dragRel(10,10, .1)
+
+    win.clickButton(qtbot, main_form, main_form.brefit)
