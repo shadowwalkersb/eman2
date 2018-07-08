2d110616c920b32b4a37e2e4e03f3549b20ede56
shadow_walker <shadowwalkersb@gmail.com>
2018-07-08 02:04:13 -0500

    markdown todo list

--- scripts/parse_indent_errors_from_byte-compilation.sh
+++ scripts/parse_indent_errors_from_byte-compilation.sh
@@ -9,10 +9,15 @@ awk '
         sub(/'\''\.*/,"", fname)
         next
     }
-    /Sorry: TabError:/{
+    /Sorry: /{
+        description = $2
         sub(/.* line /,"")
         sub(/\)/,"")
-        printf("%s:%d\n", fname, $0 )
+        
+        gh_link = "../tree/master/" fname "#L" $0
+
+        printf("- [ ] %s [%s:%d](%s)\n", description, fname, $0, gh_link)
+
         next
     }
 '
