16d5477f8595b1907d80e94a824fd7e3f162d883
shadow_walker <shadowwalkersb@gmail.com>
2018-07-29 13:20:03 -0500

    update git auto

--- convertQT4toQT5.py
+++ convertQT4toQT5.py
@@ -1,5 +1,6 @@
 import os, re, stat
 from os.path import normpath
+import subprocess
 
 from PyQt5 import QtWidgets, QtCore
 main_path = '.'
@@ -10,7 +11,7 @@ qtCore_modules = dir(QtCore)
 dictWidgets={}
 dictCore={}
 
-def fixFile(line_list):
+def fixFile(line_list, module_to_update):
 	line_generator = list(line_list)
 	changed = False
 	num = -1
@@ -24,7 +25,7 @@ def fixFile(line_list):
 
 		result = re.search('QtGui\.(\w+)', each)
 
-		if result and result.group(1) in qtWidgets_modules:
+		if result and result.group(1) == module_to_update:
 			line_list[num] = each.replace('QtGui', 'QtWidgets')
 			changed = True
 			dictWidgets[result.group(1)] = 1
@@ -33,43 +34,48 @@ def fixFile(line_list):
 			# else:
 			# 	dictWidgets[result.group(1)] += 1
 
-		if result and result.group(1) in qtCore_modules:
-			line_list[num] = each.replace('QtGui', 'QtCore')
-			changed = True
-			dictCore[result.group(1)] = 1
-			# if result.group(1) not in qtCore_modules:
-			# 	dictCore[result.group(1)] = 0
-			# else:
-			# 	dictCore[result.group(1)] += 1
+		# if result and result.group(1) in qtCore_modules:
+		# 	line_list[num] = each.replace('QtGui', 'QtCore')
+		# 	changed = True
+		# 	dictCore[result.group(1)] = 1
+		# 	# if result.group(1) not in qtCore_modules:
+		# 	# 	dictCore[result.group(1)] = 0
+		# 	# else:
+		# 	# 	dictCore[result.group(1)] += 1
 
 	if changed:
 		return ''.join(line_list)
 
-def convert(main_path):
+def convert(main_path, module_to_convert):
 	for dirs, folders, files in os.walk(main_path):
 		for each in [x for x in files if x.endswith('.py') and normpath(x) != normpath(__file__)]:
 			file_path = os.path.join(dirs, each).replace(os.sep, '/')
-			print(file_path)
+			# print(file_path)
 			# os.chmod(file_path, stat.S_IWRITE)
 
 			with open(file_path, 'r+') as out_file:
 				line_list = out_file.readlines()
 
-			fixed_lines = fixFile(line_list)
-			# 
-			# if fixed_lines:
-			# 	print file_path
-			# 
-			# 	with open(file_path, 'w+') as out_file:
-			# 		out_file.write(fixed_lines)
+			fixed_lines = fixFile(line_list, module_to_convert)
 
-convert(main_path=main_path)
+			if fixed_lines:
+				print file_path
+
+				with open(file_path, 'w+') as out_file:
+					out_file.write(fixed_lines)
+
+for k in qtWidgets_modules:
+	print(k)
+	convert('.', k)
+	subprocess.call(["git", "status"])
+	subprocess.call(["git", "add", "."])
+	subprocess.call(["git", "commit", "-m", k])
 # slf='./convertQT4toQT5.py'
 # print(slf)
 # print(__file__)
 # print(normpath(slf))
 # print(normpath(__file__))
 
-for k in dictWidgets.keys():
-	print(k)
-print(dictCore.keys())
+# for k in dictWidgets.keys():
+# 	print(k)
+# print(dictCore.keys())
