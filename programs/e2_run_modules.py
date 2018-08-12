5e93245f4000c59aadb361057a25e7dab43decc9
shadow_walker <shadowwalkersb@gmail.com>
2018-08-11 21:53:08 -0500

    e2_run_modules

---
+++ programs/e2_run_modules.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from __future__ import print_function
+from __future__ import division
+
+import sys
+
+from eman2_gui import em3Dfonts
+from eman2_gui import emboxerbase
+from eman2_gui import emimage3d
+from eman2_gui import empdbvaltool
+from eman2_gui import empdbviewer
+from eman2_gui import emsprworkflow
+
+em3Dfonts.main()
+emboxerbase.main()
+emimage3d.main()
+empdbvaltool.main()
+empdbviewer.main()
+emsprworkflow.main()
