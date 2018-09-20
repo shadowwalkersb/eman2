5a2bda2661620d9f887e1ea75d7dd338ba766771
shadow_walker <shadowwalkersb@gmail.com>
2018-09-20 15:57:08 -0500

    convertQT4toQT5.py

---
+++ scripts/convertQT4toQT5.py
@@ -0,0 +1,57 @@
+import os, re, stat
+from os.path import normpath
+import subprocess
+
+from PyQt5 import QtWidgets, QtCore
+main_path = '.'
+
+qtWidgets_modules = dir(QtWidgets)
+qtCore_modules = dir(QtCore)
+
+dictWidgets={}
+dictCore={}
+
+def fixFile(line_list, module_to_update):
+	line_generator = list(line_list)
+	changed = False
+	num = -1
+
+	for each in line_generator:
+		num += 1
+
+		result = re.search('QtGui\.(\w+)', each)
+
+		if result and result.group(1) == module_to_update and each.strip()[0] != '#':
+			line_list[num] = each.replace('QtGui.' + module_to_update, 'QtWidgets.' + module_to_update)
+			changed = True
+			dictWidgets[result.group(1)] = 1
+
+	if changed:
+		return ''.join(line_list)
+
+def convert(main_path, module_to_convert):
+	for dirs, folders, files in os.walk(main_path):
+		for each in [x for x in files if x.endswith('.py') and normpath(x) != normpath(__file__)]:
+			file_path = os.path.join(dirs, each).replace(os.sep, '/')
+			# print(file_path)
+			if normpath(file_path).startswith('sparx'):
+				continue
+			# os.chmod(file_path, stat.S_IWRITE)
+
+			with open(file_path, 'r+') as out_file:
+				line_list = out_file.readlines()
+
+			fixed_lines = fixFile(line_list, module_to_convert)
+
+			if fixed_lines:
+				print file_path
+
+				with open(file_path, 'w+') as out_file:
+					out_file.write(fixed_lines)
+
+for k in qtWidgets_modules:
+	print(k)
+	convert('.', k)
+	subprocess.call(["git", "status"])
+	subprocess.call(["git", "add", "."])
+	subprocess.call(["git", "commit", "-m", k])
