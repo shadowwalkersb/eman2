abc9120d87b449e05477dec58cc56823ebca75e6
shadow_walker <shadowwalkersb@gmail.com>
2019-03-24 23:56:00 -0500

    CMAKE_SYSROOT in toolchain file

--- recipes/eman/cross-linux.cmake
+++ recipes/eman/cross-linux.cmake
@@ -9,6 +9,9 @@ set(CMAKE_CXX_COMPILER $ENV{CXX})
 # where is the target environment
 set(CMAKE_FIND_ROOT_PATH $ENV{PREFIX} $ENV{BUILD_PREFIX}/$ENV{HOST}/sysroot)
 
+# https://gitlab.kitware.com/cmb/smtk/blob/9bca96bb340e6531bd7e0597a83dda12681145e8/conda/CondaToolchain.cmake#L4
+set(CMAKE_SYSROOT "${conda_root}")
+
 # search for programs in the build host directories
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 # for libraries and headers in the target directories
