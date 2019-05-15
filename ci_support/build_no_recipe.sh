1d0a87423224f5b10bd85dd10f6871109cee1099
shadow_walker <shadowwalkersb@gmail.com>
2019-05-15 15:13:11 -0500

    eman-deps=15

--- ci_support/build_no_recipe.sh
+++ ci_support/build_no_recipe.sh
@@ -10,7 +10,7 @@ source ci_support/setup_conda.sh
 
 # Following Wiki instructions at
 # http://blake.bcm.edu/emanwiki/EMAN2/COMPILE_EMAN2_ANACONDA
-conda install eman-deps=14.0 -c cryoem -c defaults -c conda-forge --yes --quiet
+conda install eman-deps=15 -c cryoem/label/py3 -c cryoem -c defaults -c conda-forge --yes --quiet
 
 python -m compileall -q .
 
