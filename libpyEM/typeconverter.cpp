487ff24401b90ca0db6f3e5cc8333bb4b5b79dc0
shadow_walker <shadowwalkersb@gmail.com>
2019-07-20 23:34:49 -0500

    numpy2em ???

--- libpyEM/typeconverter.cpp
+++ libpyEM/typeconverter.cpp
@@ -140,14 +140,14 @@ EMData* EMNumPy::numpy2em(const python::numeric::array& array)
 	EMData* image = 0;
 	float * temparray = new float[(size_t)nx*ny*nz];
 	if(data_type == 'f') {
-		void* array_data = PyArray_DATA(array_ptr);
-		memcpy(temparray, array_data, (size_t)nx*ny*nz*sizeof(float));
+		double* array_data = (double*)PyArray_DATA(array_ptr);
+		std::copy(array_data, array_data + (size_t)nx*ny*nz, temparray);
 		image = new EMData((float*)temparray, nx, ny, nz);
 	}
 	else {
 		PyArrayObject * array_ptr2 = (PyArrayObject*) PyArray_Cast(array_ptr, NPY_FLOAT32); //
-		void* array_data2 = PyArray_DATA(array_ptr2);
-		memcpy(temparray, array_data2, (size_t)nx*ny*nz*sizeof(float));
+		float* array_data2 = (float*)PyArray_DATA(array_ptr2);
+		std::copy(array_data2, array_data2 + (size_t)nx*ny*nz, temparray);
 		image = new EMData((float*)temparray, nx, ny, nz);
 	}
 
