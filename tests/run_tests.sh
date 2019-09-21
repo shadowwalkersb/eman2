144ebbf6af1e051e180b1df2570fca9f1790f221
shadow_walker <shadowwalkersb@gmail.com>
2019-09-20 23:25:17 -0500

    skip githash test [ci skip] [ci build]

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -11,11 +11,6 @@ e2version.py
 e2speedtest.py
 time python -c "from EMAN2 import *; Util.version()"
 
-# 2. Test git-hash consistency
-if [ ${CONDA_BUILD:-0} -ne 1 ];then
-    bash "${MYDIR}/test_git_hash.sh"
-fi
-
 # 3. Existence tests for data files like images, font files, JSON
 python "${MYDIR}/test_EMAN2DIR.py"
 
