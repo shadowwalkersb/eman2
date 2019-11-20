197b92e95254ad56e859d44cfdcac4160cd10c9d
shadow_walker <shadowwalkersb@gmail.com>
2019-11-20 13:16:42 -0600

    boost [skip travis] [skip appveyor]

--- cmake/Boost.cmake
+++ cmake/Boost.cmake
@@ -5,16 +5,24 @@ find_package(Boost COMPONENTS python numpy REQUIRED)
 message("Boost_LIBRARIES:   ${Boost_LIBRARIES}")
 message("Boost_INCLUDE_DIR: ${Boost_INCLUDE_DIR}")
 
+add_library(anaconda_compiler_include INTERFACE)
+set_target_properties(anaconda_compiler_include
+					PROPERTIES
+					INTERFACE_INCLUDE_DIRECTORIES
+					$<$<AND:$<CXX_COMPILER_ID:GNU>,$<BOOL:$ENV{CONDA_BUILD}>>:$ENV{BUILD_PREFIX}/x86_64-conda_cos6-linux-gnu/include/c++/7.3.0/>
+)
+
 #this definition is for boost.python > 1.35.0 
 set_target_properties(Boost::python
 					  PROPERTIES
 					  INTERFACE_COMPILE_DEFINITIONS BOOST_PYTHON_NO_PY_SIGNATURES
 					  INTERFACE_LINK_LIBRARIES Python::Python
-					  INTERFACE_INCLUDE_DIRECTORIES $<$<AND:$<CXX_COMPILER_ID:GNU>,$<BOOL:$ENV{CONDA_BUILD}>>:$ENV{BUILD_PREFIX}/x86_64-conda_cos6-linux-gnu/include/c++/7.3.0/>
 					  )
 if(WIN32)
 	ADD_DEFINITIONS(-DBOOST_DISABLE_ASSERTS)
 endif()
+
+target_link_libraries(Boost::python INTERFACE anaconda_compiler_include)
 
 IF(CMAKE_SYSTEM MATCHES "IRIX.*")
     INCLUDE_DIRECTORIES(${Boost_INCLUDE_DIR}/boost/compatibility/cpp_c_headers)
