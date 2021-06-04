b1f00f7fddc0d6412fc34b75a4428a5557003d14
shadow_walker <shadowwalkersb@gmail.com>
2021-06-03 21:34:18 -0500

    ss

--- libpyEM/e3argparse.py
+++ libpyEM/e3argparse.py
@@ -4,4 +4,4 @@ from EMAN2 import EMArgumentParser
 class E3ArgumentParser(EMArgumentParser):
 
     def __init__(self, prog=None,usage=None,description=None,epilog=None,parents=[],formatter_class=EMArgumentParser.HelpFormatter,prefix_chars='-',fromfile_prefix_chars=None,argument_default=None,conflict_handler='error',add_help=True,allow_abbrev=True):
-        super(E3ArgumentParser, self).__init__(prog=prog,usage=usage,description=description,epilog=epilog,parents=parents,formatter_class=formatter_class,prefix_chars=prefix_chars,fromfile_prefix_chars=fromfile_prefix_chars,argument_default=argument_default,conflict_handler=conflict_handler,add_help=add_help,allow_abbrev=allow_abbrev)
+        super(EMArgumentParser, self).__init__(prog=prog,usage=usage,description=description,epilog=epilog,parents=parents,formatter_class=formatter_class,prefix_chars=prefix_chars,fromfile_prefix_chars=fromfile_prefix_chars,argument_default=argument_default,conflict_handler=conflict_handler,add_help=add_help,allow_abbrev=allow_abbrev)
