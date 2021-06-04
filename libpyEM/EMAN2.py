61fd2e99be3697f958bb75ac24dd85a109947d97
shadow_walker <shadowwalkersb@gmail.com>
2021-06-03 20:51:07 -0500

    EMArgumentParser.nargs

--- libpyEM/EMAN2.py
+++ libpyEM/EMAN2.py
@@ -664,7 +664,7 @@ def commandoptions(options,exclude=[]):
 
 class EMArgumentParser(argparse.ArgumentParser):
 	""" subclass of argparser to masquerade as optparser and run the GUI """
-	def __init__(self, prog=None,usage=None,description=None,epilog=None,version=None,parents=[],formatter_class=argparse.HelpFormatter,prefix_chars='-',fromfile_prefix_chars=None,argument_default=None,conflict_handler='error',add_help=True,allow_abbrev=True):
+	def __init__(self, prog=None,usage=None,description=None,epilog=None,nargs=None,version=None,parents=[],formatter_class=argparse.HelpFormatter,prefix_chars='-',fromfile_prefix_chars=None,argument_default=None,conflict_handler='error',add_help=True,allow_abbrev=True):
 		argparse.ArgumentParser.__init__(self,prog=prog,usage=usage,description=description,epilog=epilog,parents=parents,formatter_class=formatter_class,prefix_chars=prefix_chars,fromfile_prefix_chars=fromfile_prefix_chars,argument_default=argument_default,conflict_handler=conflict_handler,add_help=add_help,allow_abbrev=allow_abbrev)
 
 		# A list of options to add to the GUI
@@ -673,7 +673,10 @@ class EMArgumentParser(argparse.ArgumentParser):
 
 		if version:
 			self.add_argument('--version', action='version', version=version)
-		self.add_argument("positionalargs", nargs="*")
+		if not nargs:
+			self.add_argument("positionalargs", nargs="*")
+		else:
+			self.add_argument("inputfile", nargs=nargs, type=argparse.FileType('r'))
 
 	def parse_args(self):
 		""" Masquerade as optparser parse options """
