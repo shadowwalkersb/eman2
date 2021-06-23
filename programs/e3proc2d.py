821537427b66c91f5bc9dc36d57f2690abdfc1c0
shadow_walker <shadowwalkersb@gmail.com>
2021-06-22 20:03:43 -0500

    options.split: default=1

--- programs/e3proc2d.py
+++ programs/e3proc2d.py
@@ -207,7 +207,7 @@ def main():
 	parser.add_argument("--anisotropic", type=str,action="append", help="Anisotropic scaling, stretches on one axis and compresses the orthogonal axis. Specify amount,angle. See e2evalrefine")
 	parser.add_argument("--selfcl", metavar="steps mode", type=int, nargs=2, help="Output file will be a 180x180 self-common lines map for each image.")
 	parser.add_argument("--setsfpairs",  action="store_true", help="Applies the radial structure factor of the 1st image to the 2nd, the 3rd to the 4th, etc")
-	parser.add_argument("--split", metavar="n", type=int, help="Splits the input file into a set of n output files")
+	parser.add_argument("--split", metavar="n", type=int, default=1, help="Splits the input file into a set of n output files")
 	parser.add_argument("--translate", type=str, action="append", help="Translate by x,y pixels")
 	parser.add_argument("--headertransform", type=int, choices=[0, 1], action="append", help="This will take the xform.align2d header value from each particle, and apply it. Pass 0 to perform the transform or 1 to perform the inverse.")
 	parser.add_argument("--verbose", "-v", dest="verbose", action="store", metavar="n", type=int, help="verbose level [0-9], higher number means higher level of verboseness",default=1)
@@ -475,7 +475,7 @@ def main():
 				else:
 					continue
 
-			if options.split and options.split > 1:
+			if options.split > 1:
 				outfile = outfilename_no_ext + ".%02d." % (i % options.split) + outfilename_ext
 
 			if not is_3d:
