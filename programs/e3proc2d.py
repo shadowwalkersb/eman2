33d003ffe77717fd6cd66bdd6e23729f4d862502
shadow_walker <shadowwalkersb@gmail.com>
2021-06-25 00:03:59 -0500

    never gonna run

--- programs/e3proc2d.py
+++ programs/e3proc2d.py
@@ -303,12 +303,6 @@ def main():
 			if not (EMUtil.get_image_count(infile) == 1 and gimme_image_dimensions3D(infile)[2] != 1):
 				is_out3d = True
 
-		if out_ext != inp_ext and out_ext == ".mrc":
-			if EMUtil.get_image_count(infile) == 1 and gimme_image_dimensions3D(infile)[2] != 1:
-				if not infile[0] == ":":
-					if EMUtil.get_image_count(infile) == 1:
-						if gimme_image_dimensions3D(infile)[2] == 1:
-							is_out3d = False
 		if out_ext != inp_ext and out_ext != ".mrc":
 			is_out3d = False
 
