b2eefc050e216c0420298eb0a74f44f20ab84144
shadow_walker <shadowwalkersb@gmail.com>
2021-06-24 23:15:03 -0500

    is_inp3d move up

--- programs/e3proc2d.py
+++ programs/e3proc2d.py
@@ -300,13 +300,13 @@ def main():
 				if gimme_image_dimensions3D(infile)[2] == 1:
 					is_single_2d_image = True
 
+		is_inp3d = (EMUtil.get_image_count(infile) == 1 and gimme_image_dimensions3D(infile)[2] != 1)
+
 		# num_inp_images, is_inp3d
 		if EMUtil.get_image_count(infile) == 1 and gimme_image_dimensions3D(infile)[2] != 1:
 			num_inp_images = 1
 		else:
 			num_inp_images = 2
-
-		is_inp3d = (EMUtil.get_image_count(infile) == 1 and gimme_image_dimensions3D(infile)[2] != 1)
 
 		# num_out_images, is_out3d
 		if out_ext == inp_ext:
