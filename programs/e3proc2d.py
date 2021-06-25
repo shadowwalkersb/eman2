f5c087ef8bd26823238475e26b0b54a1ae0ca461
shadow_walker <shadowwalkersb@gmail.com>
2021-06-25 04:04:03 -0500

    333

--- programs/e3proc2d.py
+++ programs/e3proc2d.py
@@ -302,8 +302,10 @@ def main():
 		if out_ext == inp_ext:
 			if infile[0] == ":":
 				is_out3d = False
+		if out_ext == inp_ext:
 			if not infile[0] == ":" and EMUtil.get_image_count(infile) == 1 and gimme_image_dimensions3D(infile)[2] != 1:
 				is_out3d = True
+		if out_ext == inp_ext:
 			if not infile[0] == ":" and not (EMUtil.get_image_count(infile) == 1 and gimme_image_dimensions3D(infile)[2] != 1):
 				is_out3d = False
 		if out_ext != inp_ext and out_ext == ".mrc":
