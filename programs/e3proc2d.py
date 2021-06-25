1e47bc2e415bccb0347a37946b46c52fecc75f6d
shadow_walker <shadowwalkersb@gmail.com>
2021-06-24 23:03:49 -0500

    ?

--- programs/e3proc2d.py
+++ programs/e3proc2d.py
@@ -301,9 +301,7 @@ def main():
 					is_single_2d_image = True
 
 		# num_inp_images, is_inp3d
-		if infile[0] == ":": 	# special flag to create a new image
-			num_inp_images = 2
-		elif EMUtil.get_image_count(infile) == 1 and gimme_image_dimensions3D(infile)[2] != 1:
+		if EMUtil.get_image_count(infile) == 1 and gimme_image_dimensions3D(infile)[2] != 1:
 			num_inp_images = 1
 		else:
 			num_inp_images = 2
