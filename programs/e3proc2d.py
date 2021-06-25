5836e4d9561e6b170203f4b083b4d32de8b89736
shadow_walker <shadowwalkersb@gmail.com>
2021-06-25 03:26:31 -0500

    2

--- programs/e3proc2d.py
+++ programs/e3proc2d.py
@@ -307,6 +307,7 @@ def main():
 			if EMUtil.get_image_count(infile) == 1:
 				if gimme_image_dimensions3D(infile)[2] != 1:
 					num_inp_images = 1
+			if EMUtil.get_image_count(infile) == 1:
 				if gimme_image_dimensions3D(infile)[2] == 1:
 					num_inp_images = 2
 
