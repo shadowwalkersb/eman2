8357543727efffcf66f9f60b3743901bd593ff6d
shadow_walker <shadowwalkersb@gmail.com>
2021-06-25 04:02:26 -0500

    disperse

--- programs/e3proc2d.py
+++ programs/e3proc2d.py
@@ -300,13 +300,12 @@ def main():
 
 		# num_out_images, is_out3d
 		if out_ext == inp_ext:
-			if infile[0] == ":": 	# special flag to create a new image
-				num_inp_images = 2
+			if infile[0] == ":":
+				is_out3d = False
 			if not infile[0] == ":" and EMUtil.get_image_count(infile) == 1 and gimme_image_dimensions3D(infile)[2] != 1:
-				num_inp_images = 1
+				is_out3d = True
 			if not infile[0] == ":" and not (EMUtil.get_image_count(infile) == 1 and gimme_image_dimensions3D(infile)[2] != 1):
-				num_inp_images = 2
-			is_out3d = (num_inp_images == 1)
+				is_out3d = False
 		if out_ext != inp_ext and out_ext == ".mrc":
 			is_out3d = True
 		if out_ext != inp_ext and out_ext != ".mrc":
