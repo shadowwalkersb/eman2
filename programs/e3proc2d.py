b0dde6ba54c884bd1c5a7ab1777486b39c0f4cb9
shadow_walker <shadowwalkersb@gmail.com>
2021-06-25 03:34:27 -0500

    move

--- programs/e3proc2d.py
+++ programs/e3proc2d.py
@@ -300,6 +300,11 @@ def main():
 				if gimme_image_dimensions3D(infile)[2] == 1:
 					is_single_2d_image = True
 
+		is_inp3d = False
+
+		if not infile[0] == ":" and EMUtil.get_image_count(infile) == 1 and gimme_image_dimensions3D(infile)[2] != 1:
+			is_inp3d = True
+
 		# num_inp_images, is_inp3d
 		if infile[0] == ":": 	# special flag to create a new image
 			num_inp_images = 2
@@ -307,10 +312,6 @@ def main():
 			num_inp_images = 1
 		elif not (EMUtil.get_image_count(infile) == 1 and gimme_image_dimensions3D(infile)[2] != 1):
 			num_inp_images = 2
-
-		is_inp3d = False
-		if not infile[0] == ":" and EMUtil.get_image_count(infile) == 1 and gimme_image_dimensions3D(infile)[2] != 1:
-			is_inp3d = True
 
 		# num_out_images, is_out3d
 		if out_ext == inp_ext:
