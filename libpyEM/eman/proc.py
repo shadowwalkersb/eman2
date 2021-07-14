a7722e5a311514b9b9a32c40f623a8c764aa2983
shadow_walker <shadowwalkersb@gmail.com>
2021-07-14 14:08:42 -0500

    sq ??/

--- libpyEM/eman/proc.py
+++ libpyEM/eman/proc.py
@@ -15,9 +15,11 @@ def parse_list_arg(*possible_types):
 
 		if not any(len(user_input_str) == k for k in types_dict.keys()):
 			raise argparse.ArgumentTypeError(f"provide {' or '.join(str(i) for i in types_dict.keys())} arguments! See --help for details.")
+		else:
+			types = types_dict[len(user_input_str)]
 
 		return [type_to_convert(usr_input_val)
 				for type_to_convert, usr_input_val
-				in zip(types_dict[len(user_input_str)], user_input_str)]
+				in zip(types, user_input_str)]
 
 	return str_to_tuple
