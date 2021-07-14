6d93409d06abfb087c502c90ec249016396c881d
shadow_walker <shadowwalkersb@gmail.com>
2021-07-13 22:17:23 -0500

    dict working ????

--- libpyEM/eman/proc.py
+++ libpyEM/eman/proc.py
@@ -3,17 +3,21 @@ import ast
 
 
 def parse_list_arg(*possible_types):
+	types_dict = {}
 	if not isinstance(possible_types[0], (tuple, list)):
-		possible_types = (possible_types,)
+		types_dict[len(possible_types)] = possible_types
+	else:
+		for i in possible_types:
+			types_dict[len(i)] = i
 
 	def str_to_tuple(s):
 		user_input_str = ast.literal_eval(s)
 
-		if not any(len(user_input_str) == len(i) for i in possible_types):
-			raise argparse.ArgumentTypeError("provide 2 or 4 arguments!")
+		if not any(len(user_input_str) == k for k in types_dict.keys()):
+			raise argparse.ArgumentTypeError(f"provide {' or '.join(str(i) for i in types_dict.keys())} arguments! See --help for details.")
 
 		return [type_to_convert(usr_input_val)
 				for type_to_convert, usr_input_val
-				in zip(possible_types, user_input_str)]
+				in zip(types_dict[len(user_input_str)], user_input_str)]
 
 	return str_to_tuple
