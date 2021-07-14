b4d1056e5b9badf5d1f183e31fe2e17cfe3fa200
shadow_walker <shadowwalkersb@gmail.com>
2021-07-14 12:20:54 -0500

    fix

--- libpyEM/eman/proc.py
+++ libpyEM/eman/proc.py
@@ -1,5 +1,4 @@
 import argparse
-import ast
 
 
 def parse_list_arg(*possible_types):
@@ -11,7 +10,7 @@ def parse_list_arg(*possible_types):
 			types_dict[len(i)] = i
 
 	def str_to_tuple(s):
-		user_input_str = ast.literal_eval(s)
+		user_input_str = s.split(',')
 
 		if not any(len(user_input_str) == k for k in types_dict.keys()):
 			raise argparse.ArgumentTypeError(f"provide {' or '.join(str(i) for i in types_dict.keys())} arguments! See --help for details.")
